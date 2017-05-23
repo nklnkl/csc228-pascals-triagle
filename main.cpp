d932795736fee54d321695dd4f7f66ad4e05dbc2
Niko Lagman <Lagman.Niko@gmail.com>
2017-05-23 14:45:06 -0400

    Center display

--- main.cpp
+++ main.cpp
@@ -12,14 +12,19 @@ int main () {
   return 0;
 }
 
-int generate (int rows) {
-  rows --;
-  int j,i,p;
-  for(i = 0; i <= rows; i++) {
-    p = 1;
-    for( j = 0; j <= i; j++ ) {
-      cout << setw(((rows) / 2) + 2) << p;
-      p = p * (i - j) / (j + 1);
+
+int generate (int n) {
+  int i, j;
+  int x;
+  for(i = 0; i < n; i++) {
+    x = 1;
+    for (j = 0; j < (n-i); j++) {
+      cout << " ";
+    }
+    for( int k = 0; k <= i; k++ ) {
+      //cout << i << endl;
+      cout << " " << x;
+      x = x * (i - k) / (k + 1);
     }
     cout << endl;
   }
